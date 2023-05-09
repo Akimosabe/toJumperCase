d8b8be0938ac37ec71fed9b74e48e3e88c6ef74a
Akimosabe <he7seven@gmail.com>
2023-05-09 19:39:52 +0500

    red square

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Игра");
@@ -8,9 +12,21 @@ public class Main {
         frame.setSize(400, 400);
         frame.setLocationRelativeTo(null);
 
-        JPanel panel = new JPanel();
-        frame.add(panel);
+        JPanel panel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.RED);
+                g.fillRect(50, 50, 50, 50);
+            }
 
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(400, 400);
+            }
+        };
+
+        frame.add(panel);
         frame.setVisible(true);
     }
 }
